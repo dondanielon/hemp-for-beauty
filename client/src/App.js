9e10599b3601c01288523f50e79764f95e546b40
Daniel Valencia <becse.ca@gmail.com>
2022-06-27 19:04:25 -0500

    avances en el navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,20 @@
 import './App.css';
 import { Route, Routes } from "react-router-dom";
 import NavBar from './components/NavBar/NavBar';
+import Home from './pages/Home/Home';
+import Products from './pages/Products/Products';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<NavBar />}>
-
+          <Route path="/" element={<Home />}/>
+          <Route path="/productos" element={<Products />} />
         </Route>
       </Routes>
     </div>
   );
-}
+};
 
 export default App;
--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from "react";
+import styles from "./NavBar.module.css";
+import MenuIcon from '@mui/icons-material/Menu';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { NavLink, Outlet } from "react-router-dom";
+import SideMenu from "../SideMenu/SideMenu";
+import Cart from "../Cart/Cart";
 
 function NavBar() {
+  const [openMenu, setOpenMenu] = useState(false);
+  const [openCart, setOpenCart] = useState(false);
+  
   return (
-    <div>NavBar</div>
-  )
+    <>
+      {openMenu && <SideMenu openMenu={openMenu} set={setOpenMenu} />}
+      {openCart && <Cart openMenu={openCart} set={setOpenCart} />}
+      <nav className={styles.container}>     
+        <button className={styles.menuButton} onClick={(e) => setOpenMenu(true)}><MenuIcon /></button> 
+        <div className={styles.mainLogoContainer}>LOGO</div>
+        <button className={styles.shopButton} onClick={(e) => setOpenCart(true)}><ShoppingCartIcon /></button>
+      </nav>
+      <Outlet />
+    </>
+    
+  );
 }
 
-export default NavBar
+export default NavBar; 
